804d581c8dd1c3c17683c8c152677b9c28f09dd3
Alvaro Grajau Rodrigues <Alvim@CRP-DESEN-GO02.crp.com>
2022-05-05 19:47:37 -0300

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './pages/acconts/login-page/login-page.component';
@@ -26,7 +27,8 @@ import { FramePageComponent } from './pages/master/frame.page';
     FramePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
